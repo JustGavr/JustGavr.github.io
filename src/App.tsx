b3b65e3b42fcbba33645ceca492ea0b13cefb47d
justgavr <krendel.mike@gmail.com>
2023-12-19 21:00:55 +0300

    Hello,world!

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Текст писать тут
+          Приветики! В рамках обучения хочу постичь реакт, чтобы догнать коллег по цеху на проекте. Ничем не владею) Опыт разработки - минимальный.
         </p>
       </header>
     </div>
